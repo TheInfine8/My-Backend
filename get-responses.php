59cfa4884d8d02d00fdfd70a55b6321c6870ccb3
TheInfine8 <theinfine8@gmail.com>
2024-09-04 11:02:58 +0530

    Fixed CORS policy to allow the correct frontend origin

--- get-responses.php
+++ get-responses.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://my-frontend-fg14.onrender.com/"); // Replace with your frontend URL
+header("Access-Control-Allow-Origin: https://my-frontend-fg14.onrender.com"); // Replace with your frontend URL
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
